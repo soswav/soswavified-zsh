14ba9740d9456912688910f1c44aefb9e4b42607
soswav <154848161+soswav@users.noreply.github.com>
2024-03-06 23:28:35 +0000

    Update plugins.zsh

--- .zsh/zinit/plugins.zsh
+++ .zsh/zinit/plugins.zsh
@@ -1,25 +1,25 @@
 ##### zinit/plugins.zsh file
 
 zinit ice wait'!0' lucid                      ## lucid mode to speed up stuff
-zinit load zdharma-continuum/fast-syntax-highlighting ## gives color to prompts, looks good and also attempts to copy the fish shell's look
+zinit light zdharma-continuum/fast-syntax-highlighting ## gives color to prompts, looks good and also attempts to copy the fish shell's look
 
 zinit ice wait'!0' lucid                      ## lucid mode to speed up stuff
-zinit light zuxfoucault/colored-man-pages_mod  ## attempts to color the man pages
+zinit light zuxfoucault/colored-man-pages_mod   ## attempts to color the man pages
 
-#zinit ice wait'!0' lucid                     ## lucid mode to speed up stuff
-#zinit light QuarticCat/zsh-smartcache         ## enhances speed on some stuff as i'm aware of
-
-zinit ice wait'!0' lucid                      ## lucid mode to speed up stuff
-zinit load marlonrichert/zsh-autocomplete      ## this is personal preference but it will basically give you (good) completion for various stuff
+zinit ice wait'!0' lucid                     ## lucid mode to speed up stuff
+zinit light QuarticCat/zsh-smartcache          ## enhances speed on some stuff as i'm aware of
 
 zinit ice wait'!0' lucid                      ## lucid mode to speed up stuff
-zinit light ianthehenry/zsh-autoquoter         ## autoquotes some stuff so you don't gotta add them manually
-
-#zinit ice wait'!0' lucid                     ## lucid mode to speed up stuff
-#zinit light chrissicool/zsh-256color          ## attemps to give your terminal more color (from what i remember)
+zinit light marlonrichert/zsh-autocomplete      ## this is personal preference but it will basically give you (good) completion for various stuff
 
 zinit ice wait'!0' lucid                      ## lucid mode to speed up stuff
-zinit load zsh-users/zsh-autosuggestions       ## previously runned commands will have a grayish look, kinda like the friendly interactive shell
+zinit light ianthehenry/zsh-autoquoter          ## autoquotes some stuff so you don't gotta add them manually
+
+zinit ice wait'!0' lucid                     ## lucid mode to speed up stuff
+zinit light chrissicool/zsh-256color           ## attemps to give your terminal more color (from what i remember)
+
+zinit ice wait'!0' lucid                      ## lucid mode to speed up stuff
+zinit light zsh-users/zsh-autosuggestions       ## previously runned commands will have a grayish look, kinda like the friendly interactive shell
 
 ### NOTE: if you want a list of plugins go to "awesome-zsh-plugins" and find one that catches your attention!
 ##### end of file
