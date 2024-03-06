52c0c5e53707ec90ccc0c23981e67b3a85a13822
soswav <154848161+soswav@users.noreply.github.com>
2024-03-06 23:27:44 +0000

    Update aliases.zsh

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -8,21 +8,24 @@ alias n="nnn"                                               ### another file man
 alias ping1="ping 1.1.1.1"                                  ### pings cloudflare's DNS (for testing my internet speed ig)
 alias ping8="ping 8.8.8.8"                                  ### pings google's DNS (also for testing my internet speed)
 alias ping9="ping 9.9.9.9"                                  ### pings quad nine's DNS (testing)
+alias vim="nvim"
+alias neovim="nvim"
+alias vi="nvim"
 
-### dirs stuff ###
+### dirs stuff
 
 function cdl() {                              ### for viewing files inside a folder (while also going inside the folder)
   if [[ -d $1 ]]; then                        ## if directory exists, then run exa, "$1" is the mentioned dir
-    cd "$1"                                ## as mentioned above, "$1" is the mentioned dir
-    exa --icons always -F -l $2            ## runs exa showing icons as a list with icons ("$2" is for selecing the "-a" or other)
+    cd "$1"                                   ## as mentioned above, "$1" is the mentioned dir
+    $DIRTOOL --icons always -F -l $2          ## runs exa showing icons as a list with icons ("$2" is for selecing the "-a" or other)
   elif [[ -a $1 ]]; then                      ## if the directory mentioned is a file, then echo to the user
-    echo -e "that's a file!"               ## prints to the terminal "that's a file!"
+    echo -e "that's a file!"                  ## prints to the terminal "that's a file!"
   else                                        ## if none of the above worked (at all) then echo
-    echo -e "there was an error"           ## prints to the terminal "there was an error"
-  fi
-}
+    echo -e "there was an error"              ## prints to the terminal "there was an error"
+  fi                                          ## end of the if statement
+}                                             ### end of function
 
-alias dirs="exa -l --icons always -F"
-alias dirsa="exa --icons always -F -la"
+alias dirs="$DIRTOOL --icons always -F -l"                       ### to see directories easier
+alias dirsa="$DIRTOOL --icons always -F -la"                     ### to see all directories easier
 
 ##### end of file
