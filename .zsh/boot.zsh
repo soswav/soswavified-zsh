814f6f511ab6f2b6cb8d05267dc378b283e2ef17
soswav <154848161+soswav@users.noreply.github.com>
2024-03-10 04:56:05 +0000

    Update boot.zsh

--- .zsh/boot.zsh
+++ .zsh/boot.zsh
@@ -1,5 +1,13 @@
 ##### boot.zsh file
 
-$FETCHTOOL
+if command -v $fetchTool >/dev/null; then ### if the "fetchTool" variable (command) inside ".zshrc" exists, then run it:
+    $fetchTool ### runs mentioned fetch tool inside ".zshrc"
+elif [[ "$notifsShow" == "true" ]]; then ### if the above didn't work but the "notifsShow" variable is set to "true" inside ".zshrc", then:
+    echo "fetch tool mentioned in \n$H/.zshrc\n could not be found! install it first!"
+fi ### closes the if statement
+
+if [[ "$isNixOS" == "true" ]]; then ### if the variable inside .zshrc called "isNixOS" has a value of "true", then run this:
+  echo -e "Nix 󱄅 loves you! Love it back!"  ### prints in the terminal "Nix 󱄅 loves you! Love it back!" you may want to install nerdfonts, though..
+fi ### closes the if statement
 
 ##### end of file
