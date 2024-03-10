e1fbe8f4f4dcef57e43df6c4eee62996816c0525
soswav <154848161+soswav@users.noreply.github.com>
2024-03-10 05:00:31 +0000

    Update plugins.zsh

--- .zsh/zinit/plugins.zsh
+++ .zsh/zinit/plugins.zsh
@@ -1,25 +1,25 @@
 ##### zinit/plugins.zsh file
 
-zinit ice wait'!0' lucid                      ## lucid mode to speed up stuff
-zinit light zdharma-continuum/fast-syntax-highlighting ## gives color to prompts, looks good and also attempts to copy the fish shell's look
+zi ice wait'!0' lucid                     ## lucid mode to speed up stuff
+zi light zdharma-continuum/fast-syntax-highlighting ## gives color to prompts, looks good and also attempts to copy the fish shell's look
 
-zinit ice wait'!0' lucid                      ## lucid mode to speed up stuff
-zinit light zuxfoucault/colored-man-pages_mod   ## attempts to color the man pages
+zi ice wait'!0' lucid                     ## lucid mode to speed up stuff
+zi light QuarticCat/zsh-smartcache          ## enhances speed on some stuff as i'm aware of
 
-zinit ice wait'!0' lucid                     ## lucid mode to speed up stuff
-zinit light QuarticCat/zsh-smartcache          ## enhances speed on some stuff as i'm aware of
+zi ice wait'!0' lucid                     ## lucid mode to speed up stuff
+zi light marlonrichert/zsh-autocomplete     ## this is personal preference but it will basically give you (good) completion for various stuff
 
-zinit ice wait'!0' lucid                      ## lucid mode to speed up stuff
-zinit light marlonrichert/zsh-autocomplete      ## this is personal preference but it will basically give you (good) completion for various stuff
+zi ice wait'!0' lucid                     ## lucid mode to speed up stuff
+zi light ianthehenry/zsh-autoquoter         ## autoquotes some stuff so you don't gotta add them manually
 
-zinit ice wait'!0' lucid                      ## lucid mode to speed up stuff
-zinit light ianthehenry/zsh-autoquoter          ## autoquotes some stuff so you don't gotta add them manually
+zi ice wait'!0' lucid                     ## lucid mode to speed up stuff
+zi light zsh-users/zsh-autosuggestions      ## previously runned commands will have a grayish look, kinda like the friendly interactive shell
 
-zinit ice wait'!0' lucid                     ## lucid mode to speed up stuff
-zinit light chrissicool/zsh-256color           ## attemps to give your terminal more color (from what i remember)
-
-zinit ice wait'!0' lucid                      ## lucid mode to speed up stuff
-zinit light zsh-users/zsh-autosuggestions       ## previously runned commands will have a grayish look, kinda like the friendly interactive shell
+zi ice wait'!0' lucid                     ## lucid mode to speed up stuff
+zi light z-shell/zsh-diff-so-fancy          ## shows diff on git cmds and some zsh stuff
 
 ### NOTE: if you want a list of plugins go to "awesome-zsh-plugins" and find one that catches your attention!
+### ANOTHER NOTE: we use "zi" here as it is a shorthand for "zinit"; kindof like an alias!
+### if you're asking "why?" then take a look at this post: https://reddit.invak.id/r/zsh/comments/sqsni7/zshellzi_vs_zdharmacontinuumzinit/
+### "zini" should also work; use zi if you care about characters inside code so much like me
 ##### end of file
