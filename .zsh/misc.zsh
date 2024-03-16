f41d40f46e2c6322259b1f68619eaeab3db701e0
soswav <154848161+soswav@users.noreply.github.com>
2024-03-16 04:26:10 +0000

    Update misc.zsh

--- .zsh/misc.zsh
+++ .zsh/misc.zsh
@@ -1,4 +1,3 @@
-  GNU nano 7.2                                                                     /home/soswav/.zsh/misc.zsh                                                                               
 ##### misc.zsh file
 
 HISTFILE=1000000              ## i forgot
@@ -6,9 +5,5 @@ SAVEHIST=1000000              ## i also forgot
 HISTFILE="$Z/.history/cache"  ## history file
 
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$H/.local/bin
-
-if [[ "$soswavTheme" == "true" ]]; then
-  PS1='%F{red}[%f%F{yellow}%n%f%F{green}@%f%F{blue}%m%f %F{171}%~%f%F{red}]%f$ '
-fi
 
 ##### end of file
